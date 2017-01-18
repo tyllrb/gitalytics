47c05b9cd8b9d79162829b45019b12e1b57a81c1
Tyler Borchert <tyler.borchert@rangle.io>
2017-01-18 14:26:09 -0500

    dero

---
+++ gitalytics/app/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () =>
+  <div class="header">
+    <div class="row">
+      <div class="col12">
+        <div class="logo">
+          <i class="fa fa-cogs" aria-hidden="true" style="margin-right: 5px;" /> gitalytics
+        </div>
+      </div>
+    </div>
+  </div>;
+
+export default Header;
